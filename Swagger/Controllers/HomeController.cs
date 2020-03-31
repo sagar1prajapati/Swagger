888e3707cecb64a9f991c798db9180b5eb8730b7
sagarprajapati2010 <sagarprajapati2010@gmail.com>
2020-03-31 11:38:01 +0530

    new home controller added

---
+++ Swagger/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> HomeGet()
+        {
+            return new string[] { "value7", "value6" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> HomeGet(int id)
+        {
+            return "value :" + id;
+        }
+    }
+}
